5fe5dbc13e2c0718200a49077eea2c8772c49207
Andy Weekes <userthreethreefive@gmail.com>
2019-02-15 13:41:48 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace divider_by_two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2
+            var newscript = new SeparateScriptBecauseJanSaidSo();
+            bool goodInputReceived = false;
+            while (!goodInputReceived)
+            {
+                try
+                {
+                    //3
+                    Console.Write("You are asked to enter a number: ");
+                    var answer1 = Console.ReadLine();
+                    newscript.Halver(Convert.ToInt32(answer1));
+                    goodInputReceived = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Uhh, what ?");
+                }
+            }
+
+            Console.WriteLine("Was that as good for you as it was for me?");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SeparateScriptBecauseJanSaidSo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace divider_by_two
+{
+    class SeparateScriptBecauseJanSaidSo
+    {
+        //1
+        public void Halver(int a)
+        {
+            Console.WriteLine("Your number " + a + " divided by 2 = " + a * .5f);
+        }
+    }
+    static class StaticClass
+    {
+        //4
+        public static int HalverINT(int a)
+        {
+            Console.WriteLine("Your number " + a + " divided by 2 = " + a * .5f);
+            return Convert.ToInt32(a * .5f);
+        }
+        //5
+        public static int HalverINT(int a, int numberOfTimes)
+        {
+            for (int i = 0; i < numberOfTimes; i++)
+            {
+                Console.WriteLine("Your number " + a + " divided by 2 = " + a * .5f);
+            }
+            return Convert.ToInt32(a * .5f);
+        }
+    }
+}
